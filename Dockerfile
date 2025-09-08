dc0456350e0380c606e1a7e02c6c70a99e5b3e57
Jan <swdiag@cqadevs-Mac-Studio.local>
2025-09-08 09:50:23 +0800

    0908

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+#Docker file
+
+#步骤1:选择一个带有python 3.9环境的轻量化Linux
+FROM python:3.10-slim
+
+#步骤2:在容器中创建一个工作目录/app，作为操作台
+WORKDIR /app
+
+#步骤3:先把需求清代，即requirements 复制到docker中，利用Docker的缓存机制，只要清单不便，下一步的安装就不需要重复进行
+COPY requirements.txt .
+
+#步骤4：根据需求清单，安装依赖库
+#--no-cache-dir 选项可以减少镜像体积
+RUN pip install --no-cache-dir -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+
+#步骤5:把项目代码的所有文件拷贝到工作目录中
+COPY . .
+
+#步骤6：设置环境变量，告诉python代码启用无头模式
+ENV HEADLESS=true
+
+#步骤7:定义容器启动时，默认执行指令
+CMD ["pytest"]
+
+#docker build -t my-automation-project:1.0
+#docker run --rm -v "$(pwd)/reports":/app/reports my-automation-project:1.0
--- commom/driver_factory.py
+++ commom/driver_factory.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+try:from selenium.webdriver.chrome.service import Service as ChromeService
+except ImportError: raise ImportError("webdriver-manager库导入失败，请确保在环境中已经正确安装：pip install webdriver-manager")
 from webdriver_manager.chrome import  ChromeDriverManager
+import os
+
 
 def get_driver():
     service = ChromeService(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    if os.getenv("HEADLESS") == "true":
+        print("---检测到HEADLESS环境变量，以无头模式运行chrome")
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless") #无头模式，不会启动浏览器窗口
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(service=service,options=options)
     driver.maximize_window()
